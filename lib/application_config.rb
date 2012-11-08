a75b1b319b8d627ffa864ee68405263378550329
Chris Legault <chrislegault2011@gmail.com>
2012-11-08 13:12:52 -0400

    updating config

--- lib/application_config.rb
+++ lib/application_config.rb
@@ -5,10 +5,10 @@ Configuration.for('application') {
   game_path 'C:/HyperSpin/Emulators/RomFiles/MAME'
 
   # Path to the Hyperspin database XML file you want search in
-  xml_path 'C:/HyperSpin/Databases/MAME/MAME.xml'
+  xml_path 'C:/HyperSpin/Databases/MAME/MAME_master.xml'
 
   # Path of file you want to output your custom list to
-  output_path 'C:/Projects/Personal/mame-scraper/MAME_custom.xml'
+  output_path 'C:/HyperSpin/Databases/MAME/MAME.xml'
 
   # Comma seperated list of extensions you want to search on
   extensions 'zip'
